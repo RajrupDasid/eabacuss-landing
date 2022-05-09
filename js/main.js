3aba940afb8840d35b6d473a6632129cc49a9aef
RajrupDasid <rajrupdasid@outlook.com>
2022-05-09 19:24:29 +0530

    resposive helper added

--- js/main.js
+++ js/main.js
@@ -1,7 +1,13 @@
 let menu=document.querySelector('.menu-icon');
+let navbar=document.querySelector('.navbar');
 
 menu.onclick=()=>{
-    menu.classList.toggle("move")
+    navbar.classList.toggle("open-menu")
+    menu.classList.toggle("move");
+}
+window.onscroll=()=>{
+    navbar.classList.remove("open-menu");
+    menu.classList.remove("move")
 }
 //Reviews swiper
 var swiper=new Swiper(".reviews-content",{
@@ -58,3 +64,13 @@ function success(){
         icon: "success",
     });
 }
+//Header Background change on scroll
+let header=document.querySelector('header');
+window.addEventListener('scroll',()=>{
+    header.classList.toggle("header-active",window.scrollY > 0);
+})
+//scroll -top button
+let scrollTop=document.querySelector('.scroll-top');
+window.addEventListener('scroll',()=>{
+    scrollTop.classList.toggle("scroll-active",window.scrollY >= 400);
+})
